261728e9abd81559252484b5de3c67c76c65db4c
unknown <Administrador@RT19728.CASATORO.LOC>
2021-03-16 18:47:13 -0500

    commit inicial

---
+++ resources/router.js
@@ -0,0 +1,78 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+// Pages
+import NotFound from './views/NotFound'
+import Login from './views/Login'
+import Logout from './views/Logout'
+import Dashboard from './views/Dashboard'
+import Clients from   './views/Clients'
+import Tariffs from './views/Tariffs'
+import ParkingLots from './views/ParkingLots'
+
+
+
+// Routes
+const router = new VueRouter({
+    mode: 'history',
+    linkActiveClass: 'is-active',
+    routes: [
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+        },
+        {
+            path: '/logout',
+            name: 'logout',
+            component: Logout,
+            meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: '/dashboard',
+            name: 'dashboard',
+            component: Dashboard,
+            meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: '/clients',
+            name: 'clients',
+            component: Clients,
+            meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: '/tariffs',
+            name: 'tariffs',
+            component: Tariffs,
+            meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: '/parking-lots',
+            name: 'parking-lots',
+            component: ParkingLots,
+            meta: {
+                requiresAuth: true,
+            }
+        },
+        { 
+            path: '/404', 
+            name: '404', 
+            component: NotFound,
+        },
+        { 
+            path: '*', 
+            redirect: '/404', 
+        },
+    ],
+});
+
+export default router
